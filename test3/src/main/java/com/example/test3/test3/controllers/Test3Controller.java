13fbb7036ca3da7f5ecc171a24a79947243c3359
rahavmagen <38604619+rahavmagen@users.noreply.github.com>
2019-06-10 19:39:07 +0300

    Add files via upload

--- test3/src/main/java/com/example/test3/test3/controllers/Test3Controller.java
+++ test3/src/main/java/com/example/test3/test3/controllers/Test3Controller.java
@@ -3,10 +3,13 @@ package com.example.test3.test3.controllers;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,12 +39,13 @@ public class Test3Controller {
 		return persons;
 
 	}
-	
-	//http://localhost:8080/crypto/ENG/EUR
+
+	// http://localhost:8080/crypto/ENG/EUR
 
 	@RequestMapping(value = "crypto/{lang}/{anotherlang}", produces = "application/json")
 
-	public String getCryptoByLang(@PathVariable("lang" )String language , @PathVariable("anotherlang") String anotherLang) {
+	public String getCryptoByLang(@PathVariable("lang") String language,
+			@PathVariable("anotherlang") String anotherLang) {
 
 		System.out.println("lang is " + language);
 		System.out.println("another lang is " + anotherLang);
@@ -50,15 +54,25 @@ public class Test3Controller {
 
 	}
 
-	//http://localhost:8080/crypto?lang=ENG&lang2=Heb
+	// http://localhost:8080/crypto?lang=ENG&lang2=Heb
 	@RequestMapping(value = "crypto")
 
-	public String getParametersFromUrl(@RequestParam HashMap<String , String> prm) {
+	public String getParametersFromUrl(@RequestParam HashMap<String, String> prm) {
 
-		System.out.println("prm are  " +prm.get("lang2") );
-		
+		System.out.println("prm are  " + prm.get("lang2"));
 
 		return personService.getCryptoByPrm(prm);
+
+	}
+
+	@RequestMapping(value = "checkPost", method = RequestMethod.POST ,  produces = "application/json")
+	public String checkPost(@RequestBody String request) {
+
+		
+		System.out.println("111111111111");
+		System.out.println("request is " + request);
+		JSONObject jsonObject = new JSONObject(request);
+		return jsonObject.get("USD").toString();
 
 	}
 
